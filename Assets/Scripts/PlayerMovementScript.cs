008b323c6f0ea22b249c012cd34f7ccfcc034283
FlamingoeZpZ <gabekotton@gmail.com>
2021-10-09 20:11:35 -0400

    The map and base code

---
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float jumpHeight = 5f;
+    public float gravity = -9.8f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    //Check what it shoud collide with
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Creates a tiny sphere that returns whether it colldies with anything not whitelisted.
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            //Forces player on ground
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        //Don't define a new vector as those are global co-ordinates
+        Vector3 move = transform.right * x + transform.forward * z; // Similar to Unreal
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButton("Jump") && isGrounded)
+        {
+            //Expensive op
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Game-Jam-Submission-Deepwoken
- 
+# GameJamGame
